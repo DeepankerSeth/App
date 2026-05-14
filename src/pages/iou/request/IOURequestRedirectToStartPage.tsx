7841b9241327d2765a3dbee006764527f1301245
Deepanker Seth <41160550+DeepankerSeth@users.noreply.github.com>
2026-05-14 02:44:18 -0700

    Clear stale request draft before quick-action redirects

--- src/pages/iou/request/IOURequestRedirectToStartPage.tsx
+++ src/pages/iou/request/IOURequestRedirectToStartPage.tsx
@@ -1,11 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import ScreenWrapper from '@components/ScreenWrapper';
+import useOnyx from '@hooks/useOnyx';
 import Navigation from '@libs/Navigation/Navigation';
 import {generateReportID} from '@libs/ReportUtils';
+import {clearMoneyRequest} from '@userActions/IOU/MoneyRequest';
 import CONST from '@src/CONST';
+import ONYXKEYS from '@src/ONYXKEYS';
 import ROUTES from '@src/ROUTES';
 import type SCREENS from '@src/SCREENS';
+import {validTransactionDraftIDsSelector} from '@src/selectors/TransactionDraft';
+import isLoadingOnyxValue from '@src/types/utils/isLoadingOnyxValue';
 import type {WithWritableReportOrNotFoundProps} from './step/withWritableReportOrNotFound';
 
 type IOURequestRedirectToStartPageProps = WithWritableReportOrNotFoundProps<typeof SCREENS.MONEY_REQUEST.START>;
@@ -17,14 +22,21 @@ function IOURequestRedirectToStartPage({
 }: IOURequestRedirectToStartPageProps) {
     const isIouTypeValid = Object.values(CONST.IOU.TYPE).includes(iouType);
     const isIouRequestTypeValid = Object.values(CONST.IOU.REQUEST_TYPE).includes(iouRequestType);
+    const hasRedirectedRef = useRef(false);
+    const [draftTransactionIDs, draftTransactionsMetadata] = useOnyx(ONYXKEYS.COLLECTION.TRANSACTION_DRAFT, {
+        selector: validTransactionDraftIDsSelector,
+    });
 
     useEffect(() => {
-        if (!isIouTypeValid || !isIouRequestTypeValid) {
+        if (hasRedirectedRef.current || !isIouTypeValid || !isIouRequestTypeValid || isLoadingOnyxValue(draftTransactionsMetadata)) {
             return;
         }
+        hasRedirectedRef.current = true;
 
         // Dismiss this modal because the redirects below will open a new modal and there shouldn't be two modals stacked on top of each other.
         Navigation.dismissModal();
+        // Quick-action deeplinks reuse OPTIMISTIC_TRANSACTION_ID, so clear any previous request draft before entering the new flow.
+        clearMoneyRequest(CONST.IOU.OPTIMISTIC_TRANSACTION_ID, draftTransactionIDs);
 
         // Redirect the person to the right start page using a random reportID
         const optimisticReportID = generateReportID();
@@ -45,10 +57,7 @@ function IOURequestRedirectToStartPage({
         } else if (iouRequestType === CONST.IOU.REQUEST_TYPE.TIME) {
             Navigation.navigate(ROUTES.MONEY_REQUEST_CREATE_TAB_TIME.getRoute(CONST.IOU.ACTION.CREATE, iouType, CONST.IOU.OPTIMISTIC_TRANSACTION_ID, optimisticReportID));
         }
-
-        // This useEffect should only run on mount which is why there are no dependencies being passed in the second parameter
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [draftTransactionIDs, draftTransactionsMetadata, iouRequestType, iouType, isIouRequestTypeValid, isIouTypeValid]);
 
     if (!isIouTypeValid || !isIouRequestTypeValid) {
         return (
